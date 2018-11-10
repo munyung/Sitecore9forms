c321d7d625f1c4c9008d43ed18d6d4356d77c966
velan99 <36262833+velan99@users.noreply.github.com>
2018-11-11 00:32:40 +0400

    Google recaptcha updated

---
+++ Sitecore.Scientist.Forms/Sitecore.Scientist.Forms/Fields/GoogleRecaptcha.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+
+namespace Sitecore.Scientist.Forms.Fields
+{
+    public class GoogleRecaptcha : StringInputViewModel
+    {
+        public string PublicKey { get; set; }
+
+        public string PrivateKey { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        protected override void InitItemProperties(Item item)
+        {
+            base.InitItemProperties(item);
+
+            PublicKey = StringUtil.GetString(item.Fields["Public Key"]);
+            PrivateKey = StringUtil.GetString(item.Fields["Private Key"]);
+            ErrorMessage = StringUtil.GetString(item.Fields["Error Message"]);
+        }
+
+        protected override void UpdateItemFields(Item item)
+        {
+            base.UpdateItemFields(item);
+
+            item.Fields["Public Key"].SetValue(PublicKey, true);
+            item.Fields["Private Key"].SetValue(PrivateKey, true);
+            item.Fields["Error Message"].SetValue(ErrorMessage, true);
+        }
+    }
+}
---
+++ Sitecore.Scientist.Forms/Sitecore.Scientist.Forms/Validator/CaptchaValidator.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using Sitecore.Data;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+using Sitecore.ExperienceForms.Mvc.Models.Validation;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Sitecore.Scientist.Forms.Validator
+{
+    public class CaptchaResponse
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        [JsonProperty("error-codes")]
+        public List<string> ErrorMessage { get; set; }
+    }
+
+    public class CaptchaValidator : ValidationElement<string>
+    {
+
+        public override IEnumerable<ModelClientValidationRule> ClientValidationRules
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.PrivateKey))
+                {
+                    yield break;
+                }
+            }
+        }
+
+        protected virtual string PrivateKey
+        {
+            get;
+            set;
+        }
+
+        protected virtual string Title
+        {
+            get;
+            set;
+        }
+        protected virtual string FieldName
+        {
+            get;
+            set;
+        }
+        public CaptchaValidator(ValidationDataModel validationItem) : base(validationItem)
+        {
+
+        }
+
+        public override void Initialize(object validationModel)
+        {
+            base.Initialize(validationModel);
+            StringInputViewModel stringInputViewModel = validationModel as StringInputViewModel;
+            if (stringInputViewModel != null)
+            {
+                var fieldItem = Context.Database.GetItem(ID.Parse(stringInputViewModel.ItemId));
+               if(fieldItem!=null)
+                {
+                    this.PrivateKey = fieldItem["Private Key"];
+                }
+                this.Title = stringInputViewModel.Title;
+                this.FieldName = stringInputViewModel.Name;
+            }
+        }
+
+        public override ValidationResult Validate(object value)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+            var isCaptchaValid = ValidateCaptcha((string)value, this.PrivateKey);
+            if (!isCaptchaValid)
+            {
+                return new ValidationResult(this.FormatMessage(new object[] { this.Title }));
+            }
+            return ValidationResult.Success;
+        }
+        public static bool ValidateCaptcha(string response, string secret)
+        {
+            var client = new WebClient();
+            var reply = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secret, response));
+
+            var captchaResponse = JsonConvert.DeserializeObject<CaptchaResponse>(reply);
+
+            return System.Convert.ToBoolean(captchaResponse.Success);
+        }
+    }
+}
