eaf77c166ef192185e9e28e7c13676b7686ce4f0
velan99 <36262833+velan99@users.noreply.github.com>
2018-11-19 14:50:16 +0400

    SendEmail submit action added

---
+++ Sitecore.Scientist.Forms/Sitecore.Scientist.Forms/SubmitActions/SendEmail.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Analytics;
+using Sitecore.Analytics.Model;
+using Sitecore.DependencyInjection;
+using Sitecore.Diagnostics;
+using Sitecore.EmailCampaign.Cd.Services;
+using Sitecore.EmailCampaign.Model.Messaging;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using Sitecore.XConnect;
+using Sitecore.XConnect.Client;
+using Sitecore.XConnect.Client.Configuration;
+using Sitecore.XConnect.Collection.Model;
+
+namespace Sitecore.Scientist.Forms.SubmitActions
+{
+
+    /// <summary>
+    /// Submit action for updating <see cref="PersonalInformation"/> and <see cref="EmailAddressList"/> facets of a <see cref="XConnect.Contact"/>.
+    /// </summary>
+    /// <seealso cref="Sitecore.ExperienceForms.Processing.Actions.SubmitActionBase{SendEmailData}" />
+    public class SendEmail : SubmitActionBase<SendEmailData>
+    {
+        private readonly IClientApiService _clientApiService;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SendEmailData"/> class.
+        /// </summary>
+        /// <param name="submitActionData">The submit action data.</param>
+        public SendEmail(ISubmitActionData submitActionData) : base(submitActionData)
+        {
+            _clientApiService= ServiceLocator.ServiceProvider.GetService<IClientApiService>();
+        }
+        
+        /// <summary>
+        /// Gets the current tracker.
+        /// </summary>
+        protected virtual ITracker CurrentTracker => Tracker.Current;
+        /// <summary>
+        /// Executes the action with the specified <paramref name="data" />.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="formSubmitContext">The form submit context.</param>
+        /// <returns><c>true</c> if the action is executed correctly; otherwise <c>false</c></returns>
+        protected override bool Execute(SendEmailData data, FormSubmitContext formSubmitContext)
+        {
+            Assert.ArgumentNotNull(data, nameof(data));
+            Assert.ArgumentNotNull(formSubmitContext, nameof(formSubmitContext));
+            var firstNameField = GetFieldById(data.FirstNameFieldId, formSubmitContext.Fields);
+            var lastNameField = GetFieldById(data.LastNameFieldId, formSubmitContext.Fields);
+            var emailField = GetFieldById(data.EmailFieldId, formSubmitContext.Fields);
+            if (firstNameField == null && lastNameField == null && emailField == null)
+            {
+                return false;
+            }
+            using (var client = CreateClient())
+            {
+                try
+                {
+                    var source = "Subcribe.Form";
+                    var id = CurrentTracker.Contact.ContactId.ToString("N");
+                    CurrentTracker.Session.IdentifyAs(source, id);
+                    var trackerIdentifier = new IdentifiedContactReference(source, id);
+                    var expandOptions = new ContactExpandOptions(
+                        CollectionModel.FacetKeys.PersonalInformation,
+                        CollectionModel.FacetKeys.EmailAddressList);
+                    Contact contact = client.Get(trackerIdentifier, expandOptions);
+                    SetPersonalInformation(GetValue(firstNameField), GetValue(lastNameField), contact, client);
+                    SetEmail(GetValue(emailField), contact, client);
+                    client.Submit();
+                    if (data.MessageId == Guid.Empty)
+                    {
+                        Logger.LogError("Empty message id");
+                        return false;
+                    }
+                    ContactIdentifier contactIdentifier = null;
+                    if (contact != null)
+                    {
+                        contactIdentifier = contact.Identifiers.FirstOrDefault<ContactIdentifier>((ContactIdentifier c) => c.IdentifierType == ContactIdentifierType.Known);
+                    }
+                    if (contactIdentifier == null)
+                    {
+                        Logger.LogError("Contact id is null.");
+                        return false;
+                    }
+                    try
+                    {
+                        _clientApiService.SendAutomatedMessage(new AutomatedMessage()
+                        {
+                            ContactIdentifier = contactIdentifier,
+                            MessageId = data.MessageId
+                        });
+                    }
+                    catch (Exception exception1)
+                    {
+                        Logger.LogError(exception1.Message, exception1);
+                    }
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError(ex.Message, ex);
+                    return false;
+                }
+            }
+        }
+       
+        /// <summary>
+        /// Creates the client.
+        /// </summary>
+        /// <returns>The <see cref="IXdbContext"/> instance.</returns>
+        protected virtual IXdbContext CreateClient()
+        {
+            return SitecoreXConnectClientConfiguration.GetClient();
+        }
+        /// <summary>
+        /// Gets the field by <paramref name="id" />.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="fields">The fields.</param>
+        /// <returns>The field with the specified <paramref name="id" />.</returns>
+        private static IViewModel GetFieldById(Guid id, IList<IViewModel> fields)
+        {
+            return fields.FirstOrDefault(f => Guid.Parse(f.ItemId) == id);
+        }
+        /// <summary>
+        /// Gets the <paramref name="field" /> value.
+        /// </summary>
+        /// <param name="field">The field.</param>
+        /// <returns>The field value.</returns>
+        private static string GetValue(object field)
+        {
+            return field?.GetType().GetProperty("Value")?.GetValue(field, null)?.ToString() ?? string.Empty;
+        }
+        /// <summary>
+        /// Sets the <see cref="PersonalInformation"/> facet of the specified <paramref name="contact" />.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="contact">The contact.</param>
+        /// <param name="client">The client.</param>
+        private static void SetPersonalInformation(string firstName, string lastName, Contact contact, IXdbContext client)
+        {
+            if (string.IsNullOrEmpty(firstName) && string.IsNullOrEmpty(lastName))
+            {
+                return;
+            }
+            PersonalInformation personalInfoFacet = contact.Personal() ?? new PersonalInformation();
+            if (personalInfoFacet.FirstName == firstName && personalInfoFacet.LastName == lastName)
+            {
+                return;
+            }
+            personalInfoFacet.FirstName = firstName;
+            personalInfoFacet.LastName = lastName;
+            client.SetPersonal(contact, personalInfoFacet);
+        }
+        /// <summary>
+        /// Sets the <see cref="EmailAddressList"/> facet of the specified <paramref name="contact" />.
+        /// </summary>
+        /// <param name="email">The email address.</param>
+        /// <param name="contact">The contact.</param>
+        /// <param name="client">The client.</param>
+        private static void SetEmail(string email, Contact contact, IXdbContext client)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return;
+            }
+            EmailAddressList emailFacet = contact.Emails();
+            if (emailFacet == null)
+            {
+                emailFacet = new EmailAddressList(new EmailAddress(email, false), "Preferred");
+            }
+            else
+            {
+                if (emailFacet.PreferredEmail?.SmtpAddress == email)
+                {
+                    return;
+                }
+                emailFacet.PreferredEmail = new EmailAddress(email, false);
+            }
+            client.SetEmails(contact, emailFacet);
+        }
+    }
+    public class SendEmailData
+    {
+        public Guid FirstNameFieldId { get; set; }
+        public Guid LastNameFieldId { get; set; }
+        public Guid EmailFieldId { get; set; }
+        public Guid MessageId { get; set; }
+       
+    }
+}
---
+++ Sitecore.Scientist.Forms/Sitecore.Scientist.Forms/sitecore/shell/client/Applications/FormsBuilder/Layouts/Actions/SendEmail.js
@@ -0,0 +1,116 @@
+﻿(function (speak) {
+    var parentApp = window.parent.Sitecore.Speak.app.findApplication('EditActionSubAppRenderer'),
+     messageParameterName = "messageId";
+        designBoardApp = window.parent.Sitecore.Speak.app.findComponent('FormDesignBoard');
+    var getFields = function () {
+        var fields = designBoardApp.getFieldsData();
+        return _.reduce(fields,
+            function (memo, item) {
+                if (item && item.model && item.model.hasOwnProperty("value")) {
+                    memo.push({
+                        itemId: item.itemId,
+                        name: item.model.name
+                    });
+                }
+                return memo;
+            },
+            [
+                {
+                    itemId: '',
+                    name: ''
+                }
+            ],
+            this);
+    };
+    speak.pageCode(["underscore"],
+        function (_) {
+            return {
+                initialized: function () {
+                    this.on({
+                        "loaded": this.loadDone
+                    },
+                        this);
+                    this.Fields = getFields();
+                    this.MapContactForm.children.forEach(function (control) {
+                        if (control.deps && control.deps.indexOf("bclSelection") !== -1) {
+                            control.IsSelectionRequired = false;
+                        }
+                    });
+                    var componentName = this.Form.bindingConfigObject[messageParameterName].split(".")[0];
+                    this.MessagesList = this.MapContactForm[componentName];
+                    this.MessagesList.on("change:SelectedItem", this.changedSelectedItemId, this);
+
+                    this.MessagesDataSource.on("change:DynamicData", this.messagesChanged, this);
+                   
+                    if (parentApp) {
+                        parentApp.loadDone(this, this.HeaderTitle.Text, this.HeaderSubtitle.Text);
+                        parentApp.setSelectability(this, true);
+                    }
+                },
+                changedSelectedItemId: function () {
+                    var isSelectable = this.MessagesList.SelectedValue && this.MessagesList.SelectedValue.length;
+                    parentApp.setSelectability(this, isSelectable);
+                },
+                setDynamicData: function (propKey) {
+                    var componentName = this.MapContactForm.bindingConfigObject[propKey].split(".")[0];
+                    var component = this.MapContactForm[componentName];
+                    var items = this.Fields.slice(0);
+                    if (this.Parameters[propKey] &&
+                        !_.findWhere(items, { itemId: this.Parameters[propKey] })) {
+                        var currentField = {
+                            itemId: this.Parameters[propKey],
+                            name: this.Parameters[propKey] +
+                                " - " +
+                                (this.ValueNotInListText.Text || "value not in the selection list")
+                        };
+                        items.splice(1, 0, currentField);
+                        component.DynamicData = items;
+                        $(component.el).find('option').eq(1).css("font-style", "italic");
+                    } else {
+                        component.DynamicData = items;
+                    }
+                },
+                setMessageData: function (listComponent, data, currentValue) {
+                    var items = data.slice(0);
+                    items.unshift({ Id: "", Name: "" });
+
+                    if (currentValue && !_.findWhere(items, { Id: currentValue })) {
+                        items.unshift({
+                            Id: "",
+                            Name: currentValue +
+                                " - " +
+                                (this.ValueNotInListText.Text || "value not in the selection list")
+                        });
+
+                        listComponent.DynamicData = items;
+                        $(listComponent.el).find('option').eq(0).css("font-style", "italic");
+                    } else {
+                        listComponent.DynamicData = items;
+                        listComponent.SelectedValue = currentValue;
+                    }
+                },
+                messagesChanged: function (items) {
+                    this.setMessageData(this.MessagesList, items, this.Parameters[messageParameterName]);
+                },
+                loadDone: function (parameters) {
+                    this.Parameters = parameters || {};
+                    _.keys(this.MapContactForm.bindingConfigObject).forEach(this.setDynamicData.bind(this));
+                    this.MapContactForm.BindingTarget = this.Parameters;
+                },
+                getData: function () {
+                    var formData = this.MapContactForm.getFormData(),
+                        keys = _.keys(formData);
+                    keys.forEach(function (propKey) {
+                        if (formData[propKey] == null || formData[propKey].length === 0) {
+                            if (this.Parameters.hasOwnProperty(propKey)) {
+                                delete this.Parameters[propKey];
+                            }
+                        } else {
+                            this.Parameters[propKey] = formData[propKey];
+                        }
+                    }.bind(this));
+                    return this.Parameters;
+                }
+            };
+        });
+})(Sitecore.Speak);
